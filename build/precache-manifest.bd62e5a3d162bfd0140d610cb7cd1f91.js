7038152cbe498dba1bf5cf2677e408a95306a1bb
Mark McMoran <mark.mcmoran@live.com>
2020-09-26 01:27:45 +0100

    Adding build/ to gh pages...

---
+++ build/precache-manifest.bd62e5a3d162bfd0140d610cb7cd1f91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2feab174c1a2d6c43bf0003ba39ce0e4",
+    "url": "/exchangely.io/index.html"
+  },
+  {
+    "revision": "043c4b3b7e33f39708c9",
+    "url": "/exchangely.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "46e6ba5947a70c731faf",
+    "url": "/exchangely.io/static/css/main.ed67b103.chunk.css"
+  },
+  {
+    "revision": "043c4b3b7e33f39708c9",
+    "url": "/exchangely.io/static/js/2.e51fafa6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/exchangely.io/static/js/2.e51fafa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e6ba5947a70c731faf",
+    "url": "/exchangely.io/static/js/main.484e8ad7.chunk.js"
+  },
+  {
+    "revision": "5c1e028262e60435fc9b",
+    "url": "/exchangely.io/static/js/runtime-main.0e580cba.js"
+  }
+]);
