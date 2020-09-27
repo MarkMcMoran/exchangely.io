fc21dc10e2e9f16f94c076b21b8e7335132b4c14
Mark McMoran <mark.mcmoran@live.com>
2020-09-27 14:02:53 +0100

    pushing build/ to master branch

---
+++ build/precache-manifest.984ccc424c7dc3df98a1d76576b01cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a83c6fbbe25405ec6cb751aa2515936",
+    "url": "/exchangely.io/index.html"
+  },
+  {
+    "revision": "641f9c6cd251bd623afb",
+    "url": "/exchangely.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a7b85eeba8b60d9b3fe6",
+    "url": "/exchangely.io/static/css/main.a9899e91.chunk.css"
+  },
+  {
+    "revision": "641f9c6cd251bd623afb",
+    "url": "/exchangely.io/static/js/2.1ee1b565.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/exchangely.io/static/js/2.1ee1b565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b85eeba8b60d9b3fe6",
+    "url": "/exchangely.io/static/js/main.baea5da3.chunk.js"
+  },
+  {
+    "revision": "5c1e028262e60435fc9b",
+    "url": "/exchangely.io/static/js/runtime-main.0e580cba.js"
+  }
+]);
